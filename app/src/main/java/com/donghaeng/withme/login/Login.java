4dcbbf95c2594e4c5b17217f8809041e37aee9cb
유건 <shine@mju.ac.kr>
2024-11-15 09:54:31 +0900

    1. 유저 객체 및 하위 상속 객체 일부 수정 2. 전화번호 인증 시 필요한 예외 처리 객체 작성 3. 로그인 과정 리팩토링 4. 리팩토링 과정에서 필요한 StartActivity 수정

--- app/src/main/java/com/donghaeng/withme/login/Login.java
+++ app/src/main/java/com/donghaeng/withme/login/Login.java
@@ -5,6 +5,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 // 로그인
 public class Login {
     private FirebaseFirestore db;
+    public Login(){
+        // 로그인 생성
+    }
     public Login(FirebaseFirestore db){
         this.db = db;
     }
--- app/src/main/java/com/donghaeng/withme/screen/start/signup/SignupPassWordFragment.java
+++ app/src/main/java/com/donghaeng/withme/screen/start/signup/SignupPassWordFragment.java
@@ -10,14 +10,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.donghaeng.withme.R;
 import com.donghaeng.withme.screen.start.StartActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import org.mindrot.jbcrypt.BCrypt;
 
 public class SignupPassWordFragment extends Fragment {
 
@@ -29,9 +24,6 @@ public class SignupPassWordFragment extends Fragment {
     TextView warningText;
     Button nextBtn;
 
-    private Timer timer;
-    private TimerTask hideWarningTask;
-
     public SignupPassWordFragment() {
         // Required empty public constructor
     }
@@ -39,14 +31,6 @@ public class SignupPassWordFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        timer = new Timer();
-        hideWarningTask = new TimerTask() {
-            @Override
-            public void run() {
-                if (warningText.getVisibility() == View.VISIBLE) warningText.setVisibility(View.INVISIBLE);
-            }
-        };
     }
 
     @Override
@@ -62,29 +46,14 @@ public class SignupPassWordFragment extends Fragment {
         startActivity = (StartActivity) requireActivity();
         warningText.setVisibility(View.INVISIBLE);
         nextBtn.setOnClickListener(new NextBtnListener());
+        startActivity.getSignUpInstance().makeAlertTask(warningText);
         return view;
     }
 
     class NextBtnListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            String pw = pwEdit.getText().toString();
-            String pwCheck = pwCheckEdit.getText().toString();
-
-            if (pw.isEmpty()) {
-                Toast.makeText(startActivity, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
-            } else if(pw.length() < 8){
-                Toast.makeText(startActivity, "비밀번호를 8자 이상 입력하세요.", Toast.LENGTH_LONG).show();
-            }else if (pwCheck.isEmpty()) {
-                Toast.makeText(startActivity, "비밀번호 확인(재입력)을 입력하세요.", Toast.LENGTH_SHORT).show();
-            } else if (!pw.equals(pwCheck)) {
-                warningText.setVisibility(View.VISIBLE);
-                timer.schedule(hideWarningTask, 5000);
-            } else {
-                String hashedPassWord = BCrypt.hashpw(pw, BCrypt.gensalt()); // 비밀번호를 해시하여 저장
-                startActivity.setUserHashedPassWord(hashedPassWord);
-                startActivity.changeFragment("SelectFragment");
-            }
+            startActivity.getSignUpInstance().checkPassWord(pwEdit, pwCheckEdit, warningText);
         }
     }
 }
