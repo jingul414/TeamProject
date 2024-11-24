1a9e80dd9c38a4fbca11225b3d2591fcb6615f85
zoejw <zoejwb03@gmail.com>
2024-11-25 03:16:52 +0900

    - '아니오' 버튼 누르면 뒤로 이동

--- app/src/main/java/com/donghaeng/withme/screen/start/connect/ConnectInfoFragment.java
+++ app/src/main/java/com/donghaeng/withme/screen/start/connect/ConnectInfoFragment.java
@@ -61,6 +61,13 @@ public class ConnectInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connect_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_connect_info, container, false);
+
+        // 아니오 버튼 클릭시 뒤로 이동
+        View back = view.findViewById(R.id.no_button);
+        back.setOnClickListener(v -> {
+            requireActivity().getOnBackPressedDispatcher().onBackPressed();
+        });
+        return view;
     }
 }
